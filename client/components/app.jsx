7a9e89bbd9aa2664748de028f959515172de6b73
Scott Elder <smelder@gmail.com>
2019-03-13 12:33:57 -0500

    First successful webpack build
    Also index.html init

---
+++ client/components/app.jsx
@@ -0,0 +1,17 @@
+import React, {Fragment, Component} from 'react';
+import ReactDOM from 'react-dom';
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return (
+      null
+    )
+  }
+}
